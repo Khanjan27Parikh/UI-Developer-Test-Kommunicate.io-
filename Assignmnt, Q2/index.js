afcd684d13a4c04dcdf824316b242aadd6c5f426
Khanjan Parikh <developerkhanju09@gmail.com>
2022-07-29 19:46:04 +0530

    final

--- Assignmnt, Q2/index.js
+++ Assignmnt, Q2/index.js
@@ -3,4 +3,14 @@ function getUrlParameterValue(url, parameter) {
     URL(url).searchParams.get(parameter);
 }
 
-console.log(getUrlParameterValue("https://www.kommunicate.io/poweredby?utm_source=https://www.kommunicate.io/&utm_medium=webplugin&utm_campaign=poweredby", "utm_medium"))
+console.log(
+  getUrlParameterValue
+    ("https://www.kommunicate.io/poweredby?utm_source=https://www.kommunicate.io/&utm_medium=webplugin&utm_campaign=poweredby", "utm_medium"
+    )
+);
+
+console.log(
+  getUrlParameterValue(
+    "https://www.kommunicate.io/poweredby?utm_source=https://www.kommunicate.io/&utm_medium=webplugin&utm_campaign=poweredby", "utm_campaign"
+  )
+);
